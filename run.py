344b6059f8891dbc4ca087a4f09ed0f682e970da
vrushtig <146242171+vrushtig@users.noreply.github.com>
2023-09-27 13:47:26 -0400

    Add files via upload

---
+++ run.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from datetime import datetime
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello world!'
+
+@app.route('/time')
+def get_time():
+    now = datetime.utcnow()
+    time = now.strftime("%Y-%m-%d %H:%M:%S")
+    return time
+
+if __name__ == '__main__':
+ app.run(host='0.0.0.0',
+        port=8080,
+        debug=True)
